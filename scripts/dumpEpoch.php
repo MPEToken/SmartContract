c8d73f1c92e72bff821ed9ab0c4dc7e766f19b9a
ZioFabry <ziofabry@hotmail.com>
2018-10-12 02:51:45 +0200

    v1.0.1

--- scripts/dumpEpoch.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$date = new DateTime('2018-10-12'); echo $date->format("Y-m-d")." => ".$date->getTimestamp()."\n";
-$date = new DateTime('2018-10-31'); echo $date->format("Y-m-d")." => ".$date->getTimestamp()."\n";
-$date = new DateTime('2018-11-26'); echo $date->format("Y-m-d")." => ".$date->getTimestamp()."\n";
-$date = new DateTime('2019-02-26'); echo $date->format("Y-m-d")." => ".$date->getTimestamp()."\n";
-
-?>
--- scripts/getLastEthPrice.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$lastPrice = json_decode( file_get_contents( 'https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR' ), TRUE );
-	if( $lastPrice === FALSE )
-	{
-	} else {
-		$line = time() . ',' . $lastPrice[0]['price_eur'] . "\n";
-		file_put_contents( 'ethprice.dat', $line, FILE_APPEND );
-	}
-?>
